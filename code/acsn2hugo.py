5967cdc0151fefb9acb8df8a83dd1430773f0986
Chloe Azencott <chloe-agathe.azencott@mines-paristech.fr>
2016-05-06 11:13:01 +0200

    Code and data to transform gene networks into SNP networks

---
+++ code/acsn2hugo.py
@@ -0,0 +1,36 @@
+# acsn2hugo.py -- Convert ACSN gene to HUGO gene
+#
+# jean-daniel.granet@mines-paristech.fr
+
+import sys
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert ACSN gene to HUGO gene')
+    parser.add_argument('acsn', help='ACSN to convert')
+    parser.add_argument('curated', help='reference file')
+    parser.add_argument('hugo', help='HUGO file to create')
+    args = parser.parse_args()
+    curated = dict()
+    # read the curated file and save into a dictionary :
+    # the key is the acsn gene name and the value is the hugo gene name
+    with open(args.curated, 'r') as fdCurated:
+        for line in fdCurated:
+            line_split = line.split('\tna\t')
+            curated[line_split[0].strip()] = line_split[1].strip()
+        fdCurated.close()
+    # read the acsn network file and write the corresponding hugo network into the output file
+    with open(args.acsn, 'r') as fdAcsn:
+        with open(args.hugo, 'w') as fdHugo:
+            for line in fdAcsn:
+                line_split = line.split('\t')
+                geneA = curated[line_split[0].strip()].split()
+                geneB = curated[line_split[2].strip()].split()
+                for elemA in geneA:
+                    for elemB in geneB:
+                        fdHugo.write("%s\n" % "\n".join(["%s %s %s" % (elemA, line_split[1], elemB)]))
+            fdHugo.close()
+        fdAcsn.close() 
+
+if __name__ == "__main__":
+    main()
---
+++ code/csv2map.py
@@ -0,0 +1,24 @@
+# csv2map.py -- Convert .csv into a .map format
+#   Description of MAP format: http://pngu.mgh.harvard.edu/~purcell/plink/data.shtml#map
+#
+# jean-daniel.granet@mines-paristech.fr
+
+import sys
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert .csv to .map')
+    parser.add_argument('csv', help='CSV file to convert')
+    parser.add_argument('map', help='MAP file to create')
+    args = parser.parse_args()
+    # read the csv file and convert it into a MAP file
+    with open(args.csv, 'r') as fdr:
+        with open(args.map, 'w') as fdw:
+            for line in fdr:
+                line_split = line.split(',')
+                fdw.write("%s\n" % "\n".join(["%s %s 0 %s" % (line_split[3], line_split[2], line_split[5])]))
+            fdw.close()
+        fdr.close()
+
+if __name__ == "__main__":
+    main()
