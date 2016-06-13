f800d11aa5a198fcb2193773b30e4e066a226321
Athenais Vaginay <vaginay.athenais@gmail.com>
2016-06-13 15:33:01 +0200

    Set resu dir and data dir

--- code/handle-output.py
+++ code/handle-output.py
@@ -4,3 +4,9 @@ import logging
 if __name__ == "__main__":
     args = sde.get_integrous_arguments_values()
 
+    for repeat_idx in xrange(args.num_repeats) : 
+        
+        resu_dir = "%s/repeat_%d" % (args.resu_dir, repeat_idx)
+        data_dir = '%s/repeat_%d' % (args.data_dir, repeat_idx)
+        
+
