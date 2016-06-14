4f9a0ffdf08a905fd42e6a5d66f6032bb324644d
Athenais Vaginay <vaginay.athenais@gmail.com>
2016-06-14 17:26:40 +0200

    Set global variables

--- code/plot.py
+++ code/plot.py
@@ -1,3 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+
+
+algos_names = ('SConES', 'MSConESnp', 'MSConES')
+num_tasks = 4
+colors = ['darkkhaki', 'royalblue', 'white']
+
