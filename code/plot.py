c1941c5fb2e97583e1d6cf83b3267593bebde1cb
Athenais Vaginay <vaginay.athenais@gmail.com>
2016-06-14 17:27:19 +0200

    Make vertical boxplots, one per algo, grouped by task

--- code/plot.py
+++ code/plot.py
@@ -7,3 +7,39 @@ algos_names = ('SConES', 'MSConESnp', 'MSConES')
 num_tasks = 4
 colors = ['darkkhaki', 'royalblue', 'white']
 
+
+
+def vertical_boxplots(data) : 
+
+    fig, axes = plt.subplots(ncols=num_tasks, sharey=True)
+    fig.subplots_adjust(wspace=0)
+    fig.canvas.set_window_title('Boxlots')
+
+    for i, (ax, task_id) in enumerate( zip(axes, xrange(num_tasks) )) : 
+        print task_id
+        # plot task per task : 
+        boxp = ax.boxplot([data[task_id][algo_id] for algo_id in algos_names], vert = True)
+        # add vertical x ticks : 
+        ax.set(xticklabels=algos_names)#, xlabel=task_id)
+        for tick in ax.get_xticklabels():
+            tick.set_rotation(45)
+        ax.margins(0.05) # Optional
+        # set colors : 
+        plt.setp(boxp['boxes'], color='black')
+        plt.setp(boxp['whiskers'], color='black')
+        plt.setp(boxp['fliers'], color='red', marker='+')
+        # set grid : 
+        add_hlines(ax) 
+
+    axes[num_tasks / 2 -1].set_title('Title')
+    #axes[num_tasks / 2].set_xlabel('Task')
+    axes[0].set_ylabel('Measure')
+
+    #make_lined_legend()
+    #make_filled_legend() 
+    
+    fig.tight_layout() #ajuste le cadrage
+    plt.savefig('boxplots1.png')   
+    plt.show()
+
+
