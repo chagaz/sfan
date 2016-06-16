a4eb8851b5f17eab7484dfe1d2eafdcff0bc4052
Athenais Vaginay <vaginay.athenais@gmail.com>
2016-06-16 16:08:17 +0200

    Use plot script to plot

--- code/handle-output.py
+++ code/handle-output.py
@@ -1,6 +1,61 @@
 import synthetic_data_experiments as sde
 import logging
+import plot
 
+
+def extract_plotable_data_from_analysis_files(f_names, num_tasks, num_repeat, num_folds):
+    """ Extract plotable data from analsis files 
+
+    Arguments
+    ---------
+    f_names : filenames
+        Path to files in the order st, np, msfan
+        holding values whith space separated lists of values :
+        -    one per task for each fold,
+             or just one per task
+        - one line per repeat
+    num_tasks: int
+        Number of tasks. 
+    num_repeat : int 
+        Number of repeat
+    num_folds : int
+        Number of folds.
+        Needed for ppv and tpr files 
+        because values per repeat are per fold then per task. 
+
+    Return
+    -------
+
+    """
+    data = {}
+    algos = ['st', 'np', 'msfan']
+    # for acc, mcc, ppv and tpr file for which values per repeat = per line 
+    # are per fold, then per task, 
+    # we have to compute means per task taking account of value per repeat and per fold. 
+    for algo_idx, algo in enumerate(algos) : 
+        data_for_an_algo  = [[float() for i in xrange(num_repeats)] for j in xrange(num_tasks)]
+        with open (f_names[algo_idx], 'r') as f : 
+            for repeat_idx, line in enumerate(f) : 
+                line_content = [float (item) for item in line.split()]
+                # line_content contains every float of the line 
+                for task_idx in xrange(num_tasks) :
+                    # we get values for the current task in content_for_a_task using a slice : 
+                    # - if data are holded one line per repeat, then per task, for each fold, 
+                    #   the slice take values stepped by num_tasks until the end of the line
+                    # - if data are holded one line per repeat, one column per task as mean for every fold, 
+                    #   then it works too, taking the task_idx th value
+                    content_for_a_task = line_content[task_idx::num_tasks] 
+                    print content_for_a_task
+                    # content_for_a_task_contains float of the current task for each folds of the current repeat
+                    # we take the mean of them to have the mean of the repeat
+                    data_for_an_algo[task_idx][repeat_idx]  = np.mean(content_for_a_task)
+            data[ algos[algo_idx] ] = data_for_an_algo
+    return data
+
+
+
+#####################################################################################################
+#####################################################################################################
 if __name__ == "__main__":
     args = sde.get_integrous_arguments_values()
 
@@ -9,6 +64,8 @@ if __name__ == "__main__":
         resu_dir = "%s/repeat_%d" % (args.resu_dir, repeat_idx)
         data_dir = '%s/repeat_%d' % (args.data_dir, repeat_idx)
         
+
+        analysis_files = sde.get_analysis_files_names(args.resu_dir, args.simu_id)
         #-----------------
         genotype_fname = '%s/%s.genotypes.txt' % (data_dir, args.simu_id)
         #------------------
@@ -270,22 +327,44 @@ if __name__ == "__main__":
     
     
     #----------------------------------------------------------------------------
-    
-    analysis_files = sde.get_analysis_files_names(args.resu_dir, args.simu_id)
-    
-    # For each measure compute average/mean +- standard deviation per task 
-    means, std = extract_res_means_and_std(analysis_files, args)
-    
-    
-    # Write means and std : 
-    fname = args.resu_dir+'/'+args.simu_id+'.results_%s'
-    print_save_res_measures(means, std, fname)
-
     # Plots : 
 
-    for measure in means : 
-        f_name = "%s/%s.%s_plot.values" %(args.resu_dir, args.simu_id, measure)
-        print_plot_files(f_name, means[measure], std[measure])
-        plot.bar_plot(measure, f_name)
-    
-    
+    # data [algo_id][task_id] = list of values
+
+    data = extract_plotable_data_from_analysis_files(
+        [analysis_files['acc_st'], analysis_files['acc_msfan_np'], analysis_files['acc_msfan'] ], 
+        args.num_tasks, args.num_repeats, args.num_folds
+    )
+    plot.horizontal_boxplots(data)
+
+    data = extract_plotable_data_from_analysis_files(
+        [analysis_files['mcc_st'], analysis_files['mcc_msfan_np'], analysis_files['mcc_msfan'] ], 
+        args.num_tasks, args.num_repeats, args.num_folds
+    )
+    plot.horizontal_boxplots(data)
+
+    data = extract_plotable_data_from_analysis_files(
+        [analysis_files['ppv_st'], analysis_files['pvv_msfan_np'], analysis_files['ppv_msfan'] ], 
+        args.num_tasks, args.num_repeats, args.num_folds
+    )
+    plot.horizontal_boxplots(data)
+
+    data = extract_plotable_data_from_analysis_files(
+        [analysis_files['tpr_st'], analysis_files['tpr_msfan_np'], analysis_files['tpr_msfan'] ], 
+        args.num_tasks, args.num_repeats, args.num_folds
+    )
+    plot.horizontal_boxplots(data)
+
+    data = extract_plotable_data_from_analysis_files(
+        [analysis_files['rmse_st'], analysis_files['rmse_msfan_np'], analysis_files['rmse_msfan'] ], 
+        args.num_tasks, args.num_repeats, args.num_folds
+    )
+    plot.horizontal_boxplots(data)
+
+    data = extract_plotable_data_from_analysis_files(
+        [analysis_files['ci_st'], analysis_files['ci_msfan_np'], analysis_files['ci_msfan'] ], 
+        args.num_tasks, args.num_repeats, args.num_folds
+    )
+    plot.horizontal_boxplots(data)
+
+
