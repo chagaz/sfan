e49e5d217f0ae310d417633f17656ec36ba0ee85
Athenais Vaginay <vaginay.athenais@gmail.com>
2016-07-11 14:58:34 +0200

    Handle ppi file with header

--- code/acsn2hugo.py
+++ code/acsn2hugo.py
@@ -32,18 +32,20 @@ def main():
 
     # read the acsn network file and write the corresponding hugo network into the output file
     with open(args.acsn_network_fname, 'r') as fdAcsn:
-        # File structure : acsn entity name 1\tName of relationship\t acsn entity name 2
+        # File structure : acsn entity name 1\tName of relationship\t acsn entity name 2 (\t PMIDS;PMIDS;PMIDS...)
+        # even if not sif and has header : no problem since no corresponding HUGO symbol will be found
         with open(args.output_fname, 'w') as fdOut:
             #File structure : hugo gene symbol\tName of relationship\thugo gene symbol
             for line in fdAcsn: # "acsn entity name 1\tName of relationship\t acsn entity name 2"
                 line_split = line.split('\t')# ["acsn entity name 1","Name of relationship","acsn entity name 2"]
                 ACSN_entity_A = line_split[0].strip()
                 ACSN_entity_B = line_split[2].strip()
-                HUGO_genes_symbols_in_ACSN_entity_A = correspondances[ACSN_entity_A]
-                HUGO_genes_symbols_in_ACSN_entity_B = correspondances[ACSN_entity_B]
-                for elemA in HUGO_genes_symbols_in_ACSN_entity_A:
-                    for elemB in HUGO_genes_symbols_in_ACSN_entity_B:
-                        fdOut.write("%s\n" % "\n".join(["%s %s %s" % (elemA, line_split[1], elemB)]))
+                HUGO_genes_symbols_in_ACSN_entity_A = correspondances.get(ACSN_entity_A)
+                HUGO_genes_symbols_in_ACSN_entity_B = correspondances.get(ACSN_entity_B)
+                if HUGO_genes_symbols_in_ACSN_entity_A and HUGO_genes_symbols_in_ACSN_entity_B : 
+                    for elemA in HUGO_genes_symbols_in_ACSN_entity_A:
+                        for elemB in HUGO_genes_symbols_in_ACSN_entity_B:
+                            fdOut.write("%s\n" % "\n".join(["%s %s %s" % (elemA, line_split[1], elemB)]))
             fdOut.close()
         fdAcsn.close() 
 
